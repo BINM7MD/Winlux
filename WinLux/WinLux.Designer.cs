e7ce9cc2f6bcbfd8c8d5931246368c4b8f970e40
BINM7MD <43062310+BINM7MD@users.noreply.github.com>
2020-09-12 23:10:11 +0300

    WinLux

---
+++ WinLux/WinLux.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace WinLux
+{
+    partial class WinLux
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WinLux));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.stat = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(56, 43);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(66, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Win";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(125, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 33);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Lux";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(297, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(53, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(224, 44);
+            this.button2.TabIndex = 4;
+            this.button2.Text = " 1- Install WSL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(53, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(224, 44);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Download Linux distro";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(53, 179);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(224, 44);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Copy XRDP commands";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // stat
+            // 
+            this.stat.AutoSize = true;
+            this.stat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stat.ForeColor = System.Drawing.Color.White;
+            this.stat.Location = new System.Drawing.Point(50, 226);
+            this.stat.Name = "stat";
+            this.stat.Size = new System.Drawing.Size(47, 13);
+            this.stat.TabIndex = 7;
+            this.stat.Text = "Status:";
+            // 
+            // WinLux
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(339, 246);
+            this.Controls.Add(this.stat);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "WinLux";
+            this.Text = "WinLux";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label stat;
+    }
+}
+
---
+++ WinLux/WinLux.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinLux
+{
+    public partial class WinLux : Form
+    {
+        public WinLux()
+        {
+            InitializeComponent();
+        }
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == WM_NCHITTEST)
+                m.Result = (IntPtr)(HT_CAPTION);
+        }
+
+        private const int WM_NCHITTEST = 0x84;
+        private const int HT_CLIENT = 0x1;
+        private const int HT_CAPTION = 0x2;
+        #region Install WSL
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("cmd.exe", "/c dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart & dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart & wsl --set-default-version 2");
+            Thread.Sleep(150);
+            stat.Text = "Status: Installed Windows Linux SubSystem!";
+            MessageBox.Show("Please Restart your PC/Laptop", "WinLux");
+
+        }
+        #endregion
+
+        #region Download Distro
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://aka.ms/wslstore");
+        }
+        #endregion
+        #region Copy commands
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("sudo apt update && sudo apt upgrade -y && sudo apt install kali-desktop-xfce -y && sudo apt install xrdp -y && sudo service xrdp start && ifconfig");
+            MessageBox.Show("Copied, After running those commands login with microsoft rdp (bulit in windows) with the ip & the user and pass that u used", "WinLux");
+        }
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
